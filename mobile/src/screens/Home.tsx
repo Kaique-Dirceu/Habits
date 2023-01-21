d896d707616a63cec80094929640931cb6bdb071
Kaique-Dirceu <dirceukaique@gmail.com>
2023-01-21 13:57:51 -0300

    mobile: add screens Habit and new

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, ScrollView } from "react-native"
 import { HabitDay, DAY_SIZE } from "../components/HabitDay"
 import { Header } from "../components/Header"
@@ -9,6 +10,8 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return(
     <View className="flex-1 bg-background px-8 pt-16">
       <Header/>
@@ -33,6 +36,7 @@ export function Home() {
           datesFromYearStart.map(date =>  (
             <HabitDay
               key={date.toISOString()}
+              onPress={() => navigate('habit', { date: date.toISOString() })}
             />
           ))
         }
